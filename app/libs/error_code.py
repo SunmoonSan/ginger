d1f28477bd1e5e183ecb1a16884cea667fa47dfb
San <18814184179@163.com>
2019-10-15 23:41:23 +0800

    错误码

--- app/libs/error_code.py
+++ app/libs/error_code.py
@@ -33,6 +33,7 @@ class ClientTypeError(APIException):
     code = 400
     msg = 'client is invalid'
     error_code = 1006
+    # description = 'client is invalid'
 
 
 class ParameterException(APIException):
