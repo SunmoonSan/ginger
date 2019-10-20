12a22ee8f7e7fda0a270e0c664ea68abb1bc0963
San <18814184179@163.com>
2019-10-20 23:57:09 +0800

    删除用户接口

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -8,7 +8,7 @@ from app.api.v1 import user, book, client, token
 def create_blueprint_v1():
     bp_v1 = Blueprint('v1', __name__)
 
-    # user.api.register(bp_v1)
+    user.api.register(bp_v1)
     # book.api.register(bp_v1)
     client.api.register(bp_v1)
     token.api.register((bp_v1))
--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @desc : Created by San on 2019/10/7 01:49
+from flask import jsonify, g
+
+from app.libs.error_code import DeleteSuccess
 from app.libs.redprint import Redprint
 from app.libs.token_auth import auth
+from app.models.base import db
+from app.models.user import User
 
 api = Redprint('user')
 
@@ -12,7 +17,22 @@ def create_user():
     pass
 
 
-@api.route('', methods=['GET'])
+@api.route('/>', methods=['GET'])
 @auth.login_required
 def get_user():
-    return 'I am Jasmine'
+    uid = g.user.uid
+    user = User.query.get_or_404(uid)
+    return jsonify(
+        id=user.id,
+        email=user.email,
+        nickname=user.nickname
+    )
+
+
+@api.route('/<uid>', methods=['DELETE'])
+@auth.login_required
+def delete_user(uid):
+    with db.auto_commit():
+        user = User.query.get_or_404(uid)
+        user.delete()
+    return DeleteSuccess()
